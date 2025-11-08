b583edf044e1a02da665a581aeefb8045298acbc
Lauchagar <lautaroo.coria@gmail.com>
2025-11-08 18:14:18 -0300

    Arreglo de validaciones

--- src/main/java/lyc/compiler/model/TypeMismatchException.java
+++ src/main/java/lyc/compiler/model/TypeMismatchException.java
@@ -1,7 +1,13 @@
 package lyc.compiler.model;
 
+import java.io.Serial;
+
 public class TypeMismatchException extends CompilerException {
-    public TypeMismatchException(String message) {
-        super(message);
-    }
+
+  @Serial
+  private static final long serialVersionUID = -8839023592847976069L;
+
+  public TypeMismatchException(String message) {
+    super("Type mismatch: " + message);
+  }
 }
